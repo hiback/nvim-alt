9b5badf7a23d1e7fadd070c6f6aee98d835f3923
hiback <5335514+hiback@users.noreply.github.com>
2024-11-10 15:42:11 +0900

    added custom cmp

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,37 @@
+local source = {}
+
+function source:complete(params, callback)
+    local items = {}
+
+    -- There's also the `cursor_after_line`, `cursor_line`, and a `cursor` fields on `context`.
+    local cursor_before_line = params.context.cursor_before_line
+
+    -- Only complete if there's a `/` anywhere before the cursor.
+    if cursor_before_line:sub(1, 1) == "#" then
+        items = {
+            { label = "#include <iostream>\n#include <vector>\n#include <string>\n\nusing namespace std;\n" },
+        }
+    end
+
+    -- `callback` should always be called.
+    callback(items)
+end
+
+-- Trigger completion (i.e. open up cmp) on these characters.
+-- We can also trigger it manually, see `:help cmp.mapping.complete`.
+function source:get_trigger_characters()
+    return { "#" }
+end
+
+-- Don't forget to register your new source to cmp.
+require("cmp").register_source("custom", source)
+
+
+return {
+    "hrsh7th/nvim-cmp",
+    opts = {
+        sources = {
+            { name = "custom" },
+        }
+    },
+}
