3c974e49c67f72f017352cd982b35b0d979a173b
hiback <5335514+hiback@users.noreply.github.com>
2024-11-10 21:23:25 +0900

    Using c++11

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,21 +4,24 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "clangd", "lua_ls", "pyright" }
+local servers = { "html", "cssls", "lua_ls", "pyright" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
+    lspconfig[lsp].setup {
+        on_attach = nvlsp.on_attach,
+        on_init = nvlsp.on_init,
+        capabilities = nvlsp.capabilities,
+    }
 end
 
 -- configuring single server, example: typescript
--- lspconfig.ts_ls.setup {
---   on_attach = nvlsp.on_attach,
---   on_init = nvlsp.on_init,
---   capabilities = nvlsp.capabilities,
--- }
+lspconfig.clangd.setup {
+    on_attach = nvlsp.on_attach,
+    on_init = nvlsp.on_init,
+    capabilities = nvlsp.capabilities,
+    init_options = {
+        fallbackFlags = { '--std=c++11' }
+    },
+}
